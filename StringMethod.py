ae6b83f4ab5ae75bc67cac421bc50887d7f33cf3
Adnannaushad112 <adnannaushad112@gmail.com>
2025-01-09 14:10:05 +0530

    Third Commit

---
+++ StringMethod.py
@@ -0,0 +1,8 @@
+s1 = 'Hello world!'
+s2 = "Hello World!"
+s3 = """Hello World!"""
+s4 = '''Python is Dynamically Typed Language'''
+print(s1)
+print(s2)
+print(s3)
+print(s4)
---
+++ UserInput.py
@@ -0,0 +1,3 @@
+num1 = input("Enter num1")
+num2 = input("Enter num2")
+print(f'Addition result of {num1} and {num2} is {num1+num2}')
